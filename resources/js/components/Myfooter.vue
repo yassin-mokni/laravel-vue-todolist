6f3a03d7e744a056f53710eea4634a69c35465c4
Yassin El Mokni <thexlearner.webdev@gmail.com>
2018-10-07 00:20:10 +0100

    first commit

---
+++ resources/js/components/Myfooter.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+            <strong>TodoList</strong> by <a href="https://fb.com/mokni.yassin">Yassine Mokni</a>
+            </p>
+        </div>
+    </footer>
+</template>
---
+++ resources/views/todo.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TodoList</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.css" />
+</head>
+<body>
+    <div id="app">
+        <Myheader></Myheader>
+        <Myfooter></Myfooter>
+    </div>
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
